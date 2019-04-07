e84a90d6335f4edcc2811b822f8c93de0cef5d26
jmc1508 <jianmingchan@gmail.com>
2019-04-07 21:25:47 +0800

    Add components to Navibar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -12,20 +12,37 @@ class Navibar extends Component {
                 <Menu fixed='top' >
                     {/* Homepage */}
                     <Menu.Item>
-                        <Icon name='home' size='large' />
+                        <img src="http://farmedhere.com.my/wp-content/uploads/2019/01/farmedhere_logo-02.png"/>
                         <Link to="#">Homepage</Link>
                     </Menu.Item>
 
-                    {/* Contact Us */}
+                    {/* About Us */}
                     <Menu.Item>
+                        <Icon name='info circle' size='large' />
+                        <Link to="#">About Us</Link>
+                    </Menu.Item>
+
+                    {/* Gallery */}
+                    <Menu.Item position='right'>
+                        <Icon name='picture' size='large' />
+                        <Link to="#">Gallery</Link>
+                    </Menu.Item>
+
+                    {/* Our Product */}
+                    <Menu.Item>
+                        <Icon name='leaf' size='large' />
+                        <Link to="#">Our Products</Link>
+                    </Menu.Item>
+
+                    {/* Contact Us */}
+                    <Menu.Item >
+                        <Icon name='mail' size='large' />
                         Contact Us
                     </Menu.Item>
-                    {/* Twitter */}
-                    <Menu.Item>
-                        Twitter
-                    </Menu.Item>
+
                     {/* Facebook */}
-                    <Menu.Item>
+                    <Menu.Item href="https://www.facebook.com/Tanjung-Nature-Farm-474159176394515/">
+                       <Icon name='facebook official' size='large' />
                         Facebook
                     </Menu.Item>
                 </Menu>
