2d375b39401397936ef701f4909217f472637f54
jmc1508 <jianmingchan@gmail.com>
2019-04-07 20:41:49 +0800

    Create Homepage.js

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                This is the homepages
+            </div>
+        );
+    }
+}
+
+export default Homepage;
